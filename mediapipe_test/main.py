a4c469ae7c4e48b7428ac9584fe3fd35246c7e1c
Piotr Haber <hpiotrek080@gmail.com>
2026-03-03 00:21:43 +0100

    working mediapipe online face landmark detection

---
+++ mediapipe_test/main.py
@@ -0,0 +1,113 @@
+# most of the code has been copied from the official mediapipe website and later only finetuded
+# for our usecase, source: https://ai.google.dev/edge/mediapipe/solutions/guide
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import drawing_utils
+from mediapipe.tasks.python.vision import drawing_styles
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    face_landmarks_list = detection_result.face_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected faces to visualize.
+    for idx in range(len(face_landmarks_list)):
+        face_landmarks = face_landmarks_list[idx]
+
+        # Draw the face landmarks.
+
+
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_TESSELATION,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_tesselation_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_contours_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_LEFT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_RIGHT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+
+    return annotated_image
+
+def plot_face_blendshapes_bar_graph(face_blendshapes):
+    # Extract the face blendshapes category names and scores.
+    face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
+    face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
+    # The blendshapes are ordered in decreasing score value.
+    face_blendshapes_ranks = range(len(face_blendshapes_names))
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+    bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores, label=[str(x) for x in face_blendshapes_ranks])
+    ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
+    ax.invert_yaxis()
+
+    # Label each bar with values
+    for score, patch in zip(face_blendshapes_scores, bar.patches):
+        plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{score:.4f}", va="top")
+
+    ax.set_xlabel('Score')
+    ax.set_title("Face Blendshapes")
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    base_options = python.BaseOptions(model_asset_path='face_landmarker_v2_with_blendshapes.task')
+    options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                        output_face_blendshapes=True,
+                                        output_facial_transformation_matrixes=True,
+                                        num_faces=1)
+    detector = vision.FaceLandmarker.create_from_options(options)
+
+    camera = cv2.VideoCapture(0)
+
+    while camera.isOpened():
+        ret, frame = camera.read()
+
+        if not ret:
+           break
+
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
+
+        detection_result = detector.detect(image)
+
+        image_copy = np.copy(image.numpy_view())
+        annotated_image = draw_landmarks_on_image(image_copy, detection_result)
+        rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+
+        cv2.imshow('mediapipe test', rgb_annotated_image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
