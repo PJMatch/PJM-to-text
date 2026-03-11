5b71b40a08668268667d83208a616852d015d5ba
jadzon <jacobtoja@icloud.com>
2026-03-11 22:36:40 +0100

    added get-task , download video file, server test TODO: add task deadline, add endpoint for sending in finished task, tbc

--- server_app/main.py
+++ server_app/main.py
@@ -1,23 +1,57 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException
 import uvicorn
 from database import Database
+from models import File, Task
+from sqlalchemy.orm import Session
 import os
+from fastapi.responses import FileResponse
 
 
-VIDEO_PATH = "./dataset/videos"
+VIDEO_PATH = os.getenv("VIDEOS_DIR", "/data/videos")
 
 
-app = FastAPI()
+def create_app(db_url: str):
 
-#Get processing status
-@app.get("/status")
-async def get_status():
-    return{
-        "health": "ok",
-        "processed": 2,
-        "total": 500
-    }
+    app = FastAPI()
+    db = Database(db_url)
 
+    @app.get("/status")
+    async def get_status(session: Session = Depends(db.get_db)):
+        return {"total": session.query(File).count()}
+
+    @app.post("/get-task")
+    async def get_task(session: Session = Depends(db.get_db)):
+        task = db.add_new_task(session)
+        if not task:
+            return {"msg": "no0 tasks"}
+    
+        session.commit()
+
+        return {
+            "task_code": task.unique_code,
+            "video_name": task.file.name,
+            "download_url": f"http://localhost:8000/download-task/{task.unique_code}"
+        }
+
+    @app.get("/download-task/{task_code}")
+    async def download_task_file(task_code: str,session: Session = Depends(db.get_db)):
+        task = session.query(Task).filter(Task.unique_code == task_code).first()
+
+        if not task:
+            raise HTTPException(status_code=404, detail="error: not found")
+        
+        file_path = task.file.path
+
+        if not os.path.exists(file_path):
+            raise HTTPException(status_code=503, detail="error: server error")
+                                
+        return FileResponse(
+        path=file_path, 
+        media_type="video/mp4", 
+        filename=task.file.name
+    )
+
+    return app, db
 
 
 def find_videos(db: Database):
@@ -46,14 +80,23 @@ def find_videos(db: Database):
         session.close()
 
 
-
 def main():
-    URL="Swag"
+    import time
 
-    db = Database(URL)
+    URL = os.getenv("DATABASE_URL", "postgresql://hivelord:hivehivehive@db:5432/hive_db")
+
+    for attempt in range(10):
+        try:
+            app, db = create_app(URL)
+            break
+        except Exception as e:
+            print(f"DB not ready ({attempt+1}/10), retrying... {e}")
+            time.sleep(3)
+    else:
+        raise RuntimeError("error - failed to connect w db")
+
     db.create_tables()
     find_videos(db)
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
